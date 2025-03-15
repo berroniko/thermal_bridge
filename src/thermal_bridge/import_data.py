db589990c42c3c17b4bced6f22cad8166ebe94c4
berroniko <mail.kresse@yahoo.de>
2025-03-15 18:47:31 +0100

    import_data.py created

---
+++ src/thermal_bridge/import_data.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+from src import DATA_DIR
+from src.berroutils.plugins.file_handler import JsonFileHandler
+from src.thermal_bridge.psi_data import Psi
+
+filepath_storage = DATA_DIR / "data/psi_data.json"
+filehandler = JsonFileHandler(file_path=filepath_storage)
+psi = Psi(filehandler=filehandler)
+
+filepath_new_source=Path("data/thermal_bridge_44er.csv")
+psi.update_from_file(filepath=filepath_new_source)

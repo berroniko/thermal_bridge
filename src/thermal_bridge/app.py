0b0fed848311da7d383f41b745a25694fe0439d8
berroniko <buzones-git@yahoo.com>
2025-04-21 21:19:14 +0200

    activated sorting according to dropdown selection

--- src/thermal_bridge/app.py
+++ src/thermal_bridge/app.py
@@ -1,13 +1,24 @@
 import pandas as pd
 import streamlit as st
 
+#  from one level above root:
+#  streamlit run /Users/niko/Python/PycharmProjects/thermal_bridge/__main__.py
 
 def streamlit_app(df):
     # Streamlit UI
     st.title("Wärmebrückendaten")
 
     waermeb_filter = st.selectbox('Wärmebrückenfilter', ['alle']+ list(df['Waermebruecke'].unique()))
-    waermeb_zusatz_filter = st.selectbox('Zusatzinfofilter', ['alle']+ list(df['Zusatzinfo Waermebruecke'].unique()))
+    if waermeb_filter != "alle":
+        df_wbf = df.loc[df['Waermebruecke'] == waermeb_filter]
+    else:
+        df_wbf = df
+    waermeb_zusatz_filter = st.selectbox('Zusatzinfofilter', ['alle']+ list(df_wbf['Zusatzinfo Waermebruecke'].unique()))
+    if waermeb_zusatz_filter != "alle":
+        df_wzf = f = df_wbf.loc[df_wbf['Zusatzinfo Waermebruecke'] == waermeb_zusatz_filter]
+    else:
+        df_wzf = df_wbf
+
 
     # User input field
     search_query = st.text_input("Bezeichnung filtern:", placeholder="z.B.: aw44 035").strip()
@@ -25,7 +36,7 @@ def streamlit_app(df):
         ]
 
     # Apply filtering
-    filtered_df = filter_data(search_query, df)
+    filtered_df = filter_data(search_query, df_wzf)
 
     # Display the table
     st.dataframe(filtered_df, use_container_width=False)

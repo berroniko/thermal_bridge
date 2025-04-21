30a79a58326928922f43609b4d1cece7f9d8110e
berroniko <buzones-git@yahoo.com>
2025-04-21 21:43:31 +0200

    (utils.py) parse_date() added

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
-def is_nan(value)->bool:
+from datetime import datetime
+
+
+def is_nan(value) -> bool:
     if isinstance(value, float):
         if value != value:
             return True
@@ -8,3 +11,12 @@ def is_nan(value)->bool:
         return True
     else:
         return False
+
+
+def parse_date(date_str: str) -> datetime:
+    for fmt in ('%d.%m.%Y', '%d/%m/%Y'):
+        try:
+            return datetime.strptime(date_str, fmt)
+        except ValueError:
+            continue
+    raise ValueError(f"Date format not supported: {date_str}")

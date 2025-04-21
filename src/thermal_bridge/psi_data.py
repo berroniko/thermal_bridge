dd99a5231ebd6253a42cd7ff286b76aef5248bc4
berroniko <buzones-git@yahoo.com>
2025-04-21 22:22:14 +0200

    (psi_data.py) parsing the "Datum" to standard format for datetime

--- src/thermal_bridge/psi_data.py
+++ src/thermal_bridge/psi_data.py
@@ -1,11 +1,13 @@
 from berroutils.container.list_of_dict_container import ListOfDictContainer
-from src.utils import is_nan
+from math import isnan
+
+from src.utils import is_nan, parse_date
 
 
 class Psi(ListOfDictContainer):
-    required_keys: set[str] = {'Waermebruecke', 'Bezeichnung', 'Psi-Wert'}
-    alternative_keys: dict[str] = {'Wärmebrücke': 'Waermebruecke',
-                                   'Zusatzinfo W\u00e4rmebr\u00fccke': 'Zusatzinfo Waermebruecke',}
+    required_keys: set[str] = {'Waermebruecke', 'Bezeichnung', 'Psi-Wert', 'Datum'}
+    alternative_keys: dict[str] = {'Wärmebrücke'                     : 'Waermebruecke',
+                                   'Zusatzinfo W\u00e4rmebr\u00fccke': 'Zusatzinfo Waermebruecke', }
 
     # format: alternative_keys = {"alt_key": "unique_key"}
 
@@ -14,4 +16,15 @@ class Psi(ListOfDictContainer):
 
     def _clean_data(self, data):
         cleaned = [elem for elem in data if not is_nan(elem.get('Bezeichnung'))]
+        cleaned = []
+        for elem in data:
+            if is_nan(elem.get('Bezeichnung')):
+                continue
+            new_elem = elem.copy()
+            datum = elem.get('Datum')
+            if is_nan(datum):
+                new_elem['Datum'] = None
+            else:
+                new_elem['Datum'] = parse_date(elem.get('Datum')).strftime("%Y-%m-%d")
+            cleaned.append(new_elem)
         return cleaned

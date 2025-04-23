17d4db12477f22f9661fedd6a9dd9718caef2b40
berroniko <buzones-git@yahoo.com>
2025-04-23 21:52:42 +0200

    (test_psi.py) generation of filepath moved to conftest.py

--- test/test_psi.py
+++ test/test_psi.py
@@ -1,13 +1,12 @@
 from berroutils.plugins.file_handler import JsonFileHandler
-from src.thermal_bridge.psi_data import Psi
+
 from src import TEST_DATA_DIR
+from src.thermal_bridge.psi_data import Psi
+from test.conftest import fp_psi_data
 
 
-filepath = TEST_DATA_DIR / "test_psi_data.json"
-filehandler = JsonFileHandler(file_path=filepath)
-
-
-def test_update_from_csv():
+def test_update_from_csv(fp_psi_data):
+    filehandler = JsonFileHandler(file_path=fp_psi_data)
     psi = Psi(filehandler=filehandler)
     filepath_new_source = TEST_DATA_DIR / "test_thermal_bridge.csv"
     psi.update_from_file(filepath=filepath_new_source)

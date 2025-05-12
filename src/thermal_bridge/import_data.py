2c231cea10a08fab2ce01148ce1c97779c8c85ca
berroniko <buzones-git@yahoo.com>
2025-05-12 21:43:38 +0200

    ready for first deployment

--- src/thermal_bridge/import_data.py
+++ src/thermal_bridge/import_data.py
@@ -6,11 +6,13 @@ from src.thermal_bridge.psi_data import Psi
 
 
 def main():
-    password = "missing"
-    mysalt = "missing"
+    password = "ebzSonne"
+    key = 'EqoEAl9UdGfWQete99CtWU-meIweaHhZE_nsYE5ekig='
+    # mykey, mysalt = Crypter.generate_key_salt_from_password(password=password)
 
     filepath = DATA_DIR / "psi_data.enc"
-    crypter = Crypter.from_password_salt(password=password, salt=mysalt)
+    crypter = Crypter(key=key)
+    # crypter = Crypter.from_password_salt(password=password, salt=mysalt)
     filehandler = CryptoJsonFileHandler(file_path=filepath, crypter=crypter)
     psi = Psi(filehandler=filehandler)
 

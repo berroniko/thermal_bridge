17ab912eb31fa4804ca75c623ad1b47050ec1b32
berroniko <buzones-git@yahoo.com>
2025-05-17 22:40:17 +0200

    (psi_data.py) added staticmethod _parse_bezeichnung()

--- src/thermal_bridge/psi_data.py
+++ src/thermal_bridge/psi_data.py
@@ -1,5 +1,6 @@
+import re
+
 from berroutils.container.list_of_dict_container import ListOfDictContainer
-from math import isnan
 
 from src.utils import is_nan, parse_date
 
@@ -19,8 +20,12 @@ class Psi(ListOfDictContainer):
         for elem in data:
             if is_nan(elem.get('Bezeichnung')):
                 continue
-            new_elem = elem.copy()
+            new_elem = {k: None if is_nan(v) else v for k, v in elem.items()}
             del new_elem["BV"]
+            del new_elem["Unnamed: 13"]
+            del new_elem["Unnamed: 14"]
+
+            new_elem = new_elem | self._parse_bezeichnung(elem.get("Bezeichnung", ""))
             if False:  # handling of date formats
                 datum = elem.get('Datum')
                 if is_nan(datum):
@@ -29,3 +34,37 @@ class Psi(ListOfDictContainer):
                     new_elem['Datum'] = parse_date(elem.get('Datum')).strftime("%Y-%m-%d")
             cleaned.append(new_elem)
         return cleaned
+
+    @staticmethod
+    def _parse_bezeichnung(bezeichnung: str) -> dict:
+        result = {
+            'staerke' : None,
+            'material': None,
+            'dichte'  : None,
+            'dicke'   : None,
+            'wlg'     : None
+        }
+
+        # Match staerke (e.g., AW44, AWEG44)
+        match_staerke = re.search(r'^([A-Z]+\d{2})', bezeichnung)
+        if match_staerke:
+            result['staerke'] = match_staerke.group()
+
+        # Match material (e.g. -P-, -V-)
+        match_material = re.search(r'-([A-Z])-', bezeichnung)
+        if match_material:
+            result['material'] = match_material.group(1)
+
+        # Match dichte (e.g., PPW2, PPW4)
+        match_dichte = re.search(r'(PPW\d)', bezeichnung)
+        if match_dichte:
+            result['dichte'] = match_dichte.group(1)
+
+        # Match all thickness and wlg pairs like 160mm032
+        thickness_match = re.search(r'(\d{2,4})mm(\d{3})', bezeichnung)
+        if thickness_match:
+            # Take the first one as per test expectation
+            result['dicke'] = int(thickness_match.group(1))
+            result['wlg'] = thickness_match.group(2)
+
+        return result
--- test/test_psi.py
+++ test/test_psi.py
@@ -1,3 +1,4 @@
+import pytest
 from berroutils.plugins.file_handler import JsonFileHandler
 
 from src import TEST_DATA_DIR
@@ -12,3 +13,57 @@ def test_update_from_csv(fp_psi_data):
     psi.update_from_file(filepath=filepath_new_source)
 
     assert len(psi.data) == 14
+    assert psi.data[0] == {'Bezeichnung'             : 'AW44-P-15KS(2,0)-160mm035-BP16(19)-060mm035US+FS-035_ohne '
+                                                       'Foamglas+ohne SD',
+                           'Datum'                   : '20.07.2017',
+                           'Farbe'                   : 'light cyan',
+                           'Name'                    : None,
+                           'Nr.'                     : 1294.0,
+                           'Psi-Wert'                : 0.2501,
+                           'VHAG'                    : 'X',
+                           'W&P'                     : None,
+                           'Waermebruecke'           : 'Außenwand auf Bodenplatte (EG)',
+                           'Zusatzinfo Waermebruecke': 'Erhöhte Terrasse (140mm028)',
+                           'dichte'                  : None,
+                           'dicke'                   : 160,
+                           'ebz'                     : None,
+                           'material'                : 'P',
+                           'mit Referenzbauteil'     : None,
+                           'staerke'                 : 'AW44',
+                           'wlg'                     : '035'}
+
+
+@pytest.mark.parametrize('bezeichnung, expected',
+                         [('AW44-P-15KS(2,0)-160mm035-BP16(19)-060mm035US+FS-035_ohne Foamglas+ohne SD',
+                           {'staerke': 'AW44', 'material': 'P', 'dichte': None, 'dicke': 160, 'wlg': '035'}),
+                          ('AWEG44-15PPW4-120mm035-stark belüftet-AWKG30-Stb-200mm037-Stb.-D16(19)',
+                           {'staerke': 'AWEG44', 'material': None, 'dichte': 'PPW4', 'dicke': 120, 'wlg': '035'}),
+                          ('AW44-P-15PPW2-160mm032-Drempel-240mm032',
+                           {'staerke': 'AW44', 'material': 'P', 'dichte': 'PPW2', 'dicke': 160, 'wlg': '032'}),
+                          ('AW44-V-15PPW2-160mm032_Stb-D18(19)+120mm042_AWEG44V-15PPW2-160mm 032-AWKG an EG',
+                           {'staerke': 'AW44', 'material': 'V', 'dichte': 'PPW2', 'dicke': 160, 'wlg': '032'}),
+                          ('AW44-P-15PPW2-160mm032_Dach-280mm032(25°)_Ringanker17,5x20cm',
+                           {'staerke': 'AW44', 'material': 'P', 'dichte': 'PPW2', 'dicke': 160, 'wlg': '032'}),
+                          ('AW44-P-15PPW2-160mm032_Dach-360mm032',
+                           {'staerke': 'AW44', 'material': 'P', 'dichte': 'PPW2', 'dicke': 160, 'wlg': '032'}),
+                          ('Dach-240mm032+40mm032',
+                           {'staerke': None, 'material': None, 'dichte': None, 'dicke': 240, 'wlg': '032'}),
+                          ('AW44-V-15KS(2,0)-160mm032-BP18(19)-100mm036US+FS ohne SD_ohne Foamglas',
+                           {'staerke': 'AW44', 'material': 'V', 'dichte': None, 'dicke': 160, 'wlg': '032'}),
+                          ('KBL-240mm035+40mm032_Dach-220mm035+40mm032',
+                           {'staerke': None, 'material': None, 'dichte': None, 'dicke': 240, 'wlg': '035'}),
+                          ('KBL-240mm035-Dach-220mm035',
+                           {'staerke': None, 'material': None, 'dichte': None, 'dicke': 240, 'wlg': '035'}),
+                          ('KBL-280mm032-Dach-360mm032   SPB beheizt',
+                           {'staerke': None, 'material': None, 'dichte': None, 'dicke': 280, 'wlg': '032'}),
+                          ('Porenbetondecke an Steildach',
+                           {'staerke': None, 'material': None, 'dichte': None, 'dicke': None, 'wlg': None}),
+                          ('240PPW4-140mm032_Dach-240mm032',
+                           {'staerke': None, 'material': None, 'dichte': 'PPW4', 'dicke': 140, 'wlg': '032'})])
+def test_parse_bezeichnung(bezeichnung, expected):
+    assert Psi._parse_bezeichnung(bezeichnung) == expected
+
+# expression = "^(?P<staerke>[A-Z]+\d+)?(-?(?P<material>[A-Z])-)?(\d+(?P<dichte>[a-zA-Z]+\d))?(-(?P<dicke>\d+)mm(?P<wlg>\d+))?"
+# gm
+# "^(?P<staerke>[A-Z]+\d+)?(-(?P<material>[A-Z]))?(-?\d+(?P<dichte>[a-zA-Z]+\d))?(-(?P<dicke>\d+)mm(?P<wlg>\d+))?"
+# gm
